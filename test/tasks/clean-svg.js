dadf447ca2f7a1f1016b6475c5a6540b0ff8f804
NIkolay Dedov <n.n.dedov@yandex.ru>
2020-03-06 01:05:39 +0300

    push new

--- test/tasks/clean-svg.js
+++
@@ -1,9 +0,0 @@
-const clean = require('gulp-clean');
-
-module.exports = (gulp, plugins, config) => {
-    return () => {
-        return gulp.src([
-            `./${config.paths.dist}/${config.output.sprite}/*`
-        ]).pipe(plugins.clean())
-    } 
-};
